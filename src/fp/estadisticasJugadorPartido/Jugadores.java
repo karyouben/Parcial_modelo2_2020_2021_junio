5b02790814b0ebdfa496b5c0ee7d3b6fbd2f1ecc
willi <willi@192.168.1.131>
2022-06-14 13:17:07 +0200

    14/06/2022

--- src/fp/estadisticasJugadorPartido/Jugadores.java
+++ src/fp/estadisticasJugadorPartido/Jugadores.java
@@ -62,7 +62,7 @@ public class Jugadores {
 	
 	//ejercicio 1
 	
-	public Boolean hayAlgunJugadorHayaMetidoPorcentajeDeTirosDe1PuntoMayorQuer(LocalDate fecha,Double r) {
+	public Boolean hayAlgunJugadorHayaMetidoPorcentajeDeTirosDe1PuntoMayorQueR(LocalDate fecha,Double r) {
 		return jugadores.stream()
 				.filter(j->j.fecha().equals(fecha) && j.tiros1()>0)
 				.anyMatch(j->j.puntos1()*100./j.tiros1()>r);
@@ -105,6 +105,60 @@ public class Jugadores {
 				res.get(clave).add(j.tiempo());
 			}
 		}return res;
+	}
+	
+	//ejercicio 4
+	
+	public List<String> listaDeLosNJugadoresMenosPuntosTirosLibres(Integer anyo,Integer n){
+		return jugadores.stream()
+				.filter(j->j.fecha().getYear()==anyo)
+				.sorted(Comparator.comparing(Jugador::puntos1))
+				.map(Jugador::nombre)
+				.distinct()
+				.limit(n)
+				.collect(Collectors.toList());
+	}
+	
+	public List<String> listaDeLosNJugadoresMenosPuntosTirosLibres2(Integer anyo,Integer n){
+		Map<String,Integer> m= jugadores.stream().filter(j->j.fecha().getYear()==anyo)
+				.collect(Collectors.groupingBy(Jugador::nombre,Collectors.summingInt(Jugador::puntos1)));
+		Comparator<Map.Entry<String, Integer>> c=Comparator.comparing(Map.Entry::getValue);
+		return m.entrySet().stream()
+				.sorted(c)
+				.map(j->j.getKey())
+				.limit(n)
+				.collect(Collectors.toList());
+	}
+	
+	//ejercicio 5
+	
+	public Map<String,List<String>> listaNombresPorPaisOrdenadosPorTiempo(){
+		return jugadores.stream()
+				.collect(Collectors.groupingBy(Jugador::pais,Collectors.collectingAndThen(Collectors.toList(), lista->listaNombreOrdenados(lista))));
+	}
+	
+	private List<String> listaNombreOrdenados(List<Jugador>lista){
+		return lista.stream()
+				.sorted(Comparator.comparing(Jugador::tiempo).reversed())
+				.map(Jugador::nombre)
+				.collect(Collectors.toList());
+				
+	}
+	
+	//ejercicio 6
+	
+	public Integer numeroDeJugadoresConMasDeNPuntos(Integer n) {
+		Map<String,Integer> m =puntosTotalesPorJugador(n);
+		return m.entrySet().stream()
+				.filter(j->j.getValue()>n)
+				.map(j->j.getKey())
+				.collect(Collectors.collectingAndThen(Collectors.counting(), j->j.intValue()));
+	}
+	
+	private Map<String,Integer> puntosTotalesPorJugador(Integer n){
+		return jugadores.stream()
+				.collect(Collectors.groupingBy(Jugador::nombre,Collectors.summingInt(Jugador::puntosTotales)));
+		
 	}
 	
 
--- src/testEstadisticasJugadorPartido/TestJugadores.java
+++ src/testEstadisticasJugadorPartido/TestJugadores.java
@@ -1,6 +1,10 @@
 package testEstadisticasJugadorPartido;
 
+import java.time.Duration;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
 
 import fp.estadisticasJugadorPartido.FactoriaJugadores;
 import fp.estadisticasJugadorPartido.Jugador;
@@ -11,9 +15,104 @@ public class TestJugadores {
 	public static void main(String[] args) {
 		List<Jugador> jugadores=FactoriaJugadores.leeJugadores("data/Jugador.csv");
 		Jugadores j=new Jugadores(jugadores);
-		System.out.println("\nTest");
-		System.out.println("======================");
+		System.out.println("\nTestHayAlgunJugadorHayaMetidoPorcentajeDeTirosDe1PuntoMayorQueR");
+		System.out.println("=================================================================");
+		testHayAlgunJugadorHayaMetidoPorcentajeDeTirosDe1PuntoMayorQueR(j,LocalDate.of(2006, 9, 16),3.2);
+		System.out.println("\nTestMediaPuntosConseguidosJugadoresPorNombre");
+		System.out.println("==============================================");
+		testMediaPuntosConseguidosJugadoresPorNombre(j,'s');
+		System.out.println("\nTestDuracionesPorPais");
+		System.out.println("=======================");
+		testDuracionesPorPais(j);
+		System.out.println("\nTestDuracionesPorPais2");
+		System.out.println("=======================");
+		testDuracionesPorPais2(j);
+		System.out.println("\nTestListaDeLosNJugadoresMenosPuntosTirosLibres");
+		System.out.println("================================================");
+		testListaDeLosNJugadoresMenosPuntosTirosLibres(j,2006,3);
+		System.out.println("\nTestListaNombresPorPaisOrdenadosPorTiempo");
+		System.out.println("================================================");
+		testListaNombresPorPaisOrdenadosPorTiempo(j);
+		System.out.println("\nTestNumeroDeJugadoresConMasDeNPuntos");
+		System.out.println("================================================");
+		testNumeroDeJugadoresConMasDeNPuntos(j,30);
+		
 
 	}
+
+
+
+	private static void testHayAlgunJugadorHayaMetidoPorcentajeDeTirosDe1PuntoMayorQueR(Jugadores j, LocalDate fecha, Double r) {
+		try {
+			String msg=String.format("¿Hay algun jugador en la fecha %d Que haya metido un porcentaje de tiros mayor que " +r+ " ?"
+					,fecha,r,j.hayAlgunJugadorHayaMetidoPorcentajeDeTirosDe1PuntoMayorQueR(fecha, r) );
+			System.out.println(msg);
+		}catch(Exception e) {
+			System.err.println("Excepcion capturada inesesperada " +e.getMessage());
+		}
+	}
+	
+	private static void testMediaPuntosConseguidosJugadoresPorNombre(Jugadores j, Character s) {
+		try {
+			String msg=String.format("La media de puntos conseguidos por jugadores que empiezan por la letra %d es: %s"
+					,s,j.mediaPuntosConseguidosJugadoresPorNombre(s) );
+			System.out.println(msg);
+		}catch(Exception e) {
+			System.err.println("Excepcion capturada inesesperada " +e.getMessage());
+		}
+	}
+	
+	private static void testDuracionesPorPais(Jugadores j) {
+		try {
+			String msg=String.format("El conjunto de duraciones de partidos por pais es: ");
+			System.out.println(msg);
+			Map<String,SortedSet<Duration>> p=j.duracionesPorPais();
+			p.entrySet().stream().forEach(System.out::println);
+		}catch(Exception e) {
+			System.err.println("Excepcion capturada inesesperada " +e.getMessage());
+		}
+	}
+	
+	private static void testDuracionesPorPais2(Jugadores j) {
+		try {
+			String msg=String.format("El conjunto de duraciones de partidos por pais es: ");
+			System.out.println(msg);
+			Map<String,SortedSet<Duration>> p=j.duracionesPorPais();
+			p.entrySet().stream().forEach(System.out::println);
+		}catch(Exception e) {
+			System.err.println("Excepcion capturada inesesperada " +e.getMessage());
+		}
+	}
+	
+	private static void testListaDeLosNJugadoresMenosPuntosTirosLibres(Jugadores j,Integer anyo, Integer n) {
+		try {
+			String msg=String.format("La Lista de los %d jugadores del año "+anyo+" con menor puntos por tiros libres son: %s "
+					,anyo,n,j.listaDeLosNJugadoresMenosPuntosTirosLibres(anyo, n) );
+			System.out.println(msg);
+		}catch(Exception e) {
+			System.err.println("Excepcion capturada inesesperada " +e.getMessage());
+		}
+	}
+	
+	private static void testListaNombresPorPaisOrdenadosPorTiempo(Jugadores j) {
+		try {
+			String msg=String.format("La lista de los nombres ordenados por tiempo en cada pais es: ");
+			System.out.println(msg);
+			Map<String,List<String>> p=j.listaNombresPorPaisOrdenadosPorTiempo();
+			p.entrySet().stream().forEach(System.out::println);
+		}catch(Exception e) {
+			System.err.println("Excepcion capturada inesesperada " +e.getMessage());
+		}
+	}
+	private static void testNumeroDeJugadoresConMasDeNPuntos(Jugadores j, Integer n) {
+		try {
+			String msg=String.format("El numero de jugadores con mas de %d puntos es: %s"
+					,n,j.numeroDeJugadoresConMasDeNPuntos(n) );
+			System.out.println(msg);
+		}catch(Exception e) {
+			System.err.println("Excepcion capturada inesesperada " +e.getMessage());
+		}
+	}
+	
 
 }
